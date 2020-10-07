d1b88e5db3f002fb65f5d6691c795ec7567c9687
Asis Perez <jose.perez2882@alumnos.udg.mx>
2020-10-06 22:17:29 -0500

    Código terminado

--- main.cpp
+++ main.cpp
@@ -6,7 +6,28 @@ int main() {
     string cadena;
     //cin >> cadena; // lee hasta espacio o \n
     getline(cin, cadena); //lee hasta \n
-    cout << cadena.size() << endl;
-
+    if(cadena == "int"){
+        cout << sizeof(int) << endl;
+    }
+    if(cadena == "char")
+    {
+        cout << sizeof(char) << endl;
+    }
+    if(cadena == "short")
+    {
+        cout << sizeof(short) << endl;
+    }
+    if(cadena == "float")
+    {
+        cout << sizeof(float) << endl;
+    }
+    if(cadena == "double")
+    {
+        cout << sizeof(double) << endl;
+    }
+    if(cadena == "long")
+    {
+        cout << sizeof(long) << endl;
+    }
     return 0;
 }
